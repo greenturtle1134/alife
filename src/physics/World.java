b0a8f3e1d1e149537ce79331f3dbe6042e914a07
greenturtle1134 <daniel.y.fu@gmail.com>
2025-09-05 11:10:22 -0400

    Put together boilerplate physics implementation

---
+++ src/physics/World.java
@@ -0,0 +1,38 @@
+package physics;
+
+import java.util.ArrayList;
+
+public class World {
+	private int width, height;
+	private ArrayList<Cell> cells;
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public World(int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.cells = new ArrayList<Cell>();
+	}
+	
+	public void addCell(Cell cell) {
+		this.cells.add(cell);
+	}
+	
+	public void tick() {
+		for (Cell c : cells) {
+			c.tick();
+			// TODO: very simple friction model
+			c.addForce(c.dx*-0.1, c.dy*-0.1);
+		}
+		
+		for (Cell c : cells) {
+			c.move();
+		}
+	}
+}
